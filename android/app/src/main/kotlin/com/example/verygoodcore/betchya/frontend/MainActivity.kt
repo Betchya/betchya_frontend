c4bc4d0ba4dcb13a92d9fc74576802c03c23255f
Josh Sanford <joshsanford1121@gmail.com>
2025-02-19 11:18:03 -0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/betchya/frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.betchya_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:betchya_frontend/app/app.dart';
+import 'package:betchya_frontend/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
